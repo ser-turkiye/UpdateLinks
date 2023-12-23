45497de1cb8c4fedb954a5670baec172e3fb7494
bbayhan <bulentbayhan@gmail.com>
2023-12-24 00:15:01 +0300

    UpdateLink first commit

---
+++ src/main/java/eng/ser/com/UpdateLinks.java
@@ -0,0 +1,249 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package eng.ser.com;
+
+import com.ser.blueline.*;
+import com.ser.blueline.metaDataComponents.*;
+import com.ser.foldermanager.*;
+import de.ser.doxis4.agentserver.UnifiedAgent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class UpdateLinks extends UnifiedAgent {
+    Logger log = LogManager.getLogger(this.getClass().getName());
+    String nameDescriptor1 = "ccmPrjDocFileName";
+    String nameDescriptorRev = "ccmPrjDocRevision";
+    String searchClassName = "Search Engineering Documents";
+    ISession ses = null;
+    IDocumentServer srv = null;
+    String prjCode = "";
+    IDescriptor descriptor1;
+    IDescriptor descriptor2;
+
+    public UpdateLinks() {
+    }
+
+    protected Object execute() {
+        this.log.info("Initiate the agent");
+        if (this.getEventDocument() == null) {
+            return this.resultError("Null Document object.");
+        } else {
+            ses = getSes();
+            srv = ses.getDocumentServer();
+
+            IDocument ldoc = this.getEventDocument();
+            try {
+                setParent(ldoc);
+                this.log.info("Finished");
+                return this.resultSuccess("Ended successfully");
+            } catch (Exception var15) {
+                throw new RuntimeException(var15);
+            }
+        }
+    }
+
+    public void setParent(IDocument engDocument){
+        this.log.info("Start Link.....");
+        String chkKeyPrnt = engDocument.getDescriptorValue("ccmPrjDocParentDoc");
+        String chkKeyCrrsInc = engDocument.getDescriptorValue("ccmPrjDocTransIncCode");
+        String chkKeyCrrsOut = engDocument.getDescriptorValue("ccmPrjDocTransOutCode");
+        this.log.info("Start Link for Parent Number:" + chkKeyPrnt + " child number:" + engDocument.getDescriptorValue("ccmPrjDocNumber"));
+        if (prjCode != null){
+            if(chkKeyPrnt != null){
+                IDocument prntDocument = getEngDocumentByNumber(ses, prjCode, chkKeyPrnt);
+                this.log.info("Parent Doc ? " + prntDocument);
+                if (prntDocument != null && !prntDocument.getDescriptorValue("ccmPrjDocCategory").trim().equalsIgnoreCase("TRANSMITTAL")) {
+                    if (!Objects.equals(prntDocument.getID(), engDocument.getID())) {
+                        ILink lnk2 = srv.createLink(ses, prntDocument.getID(), (INodeGeneric) null, engDocument.getID());
+                        lnk2.commit();
+                        this.log.info("Created Link...");
+                    }
+                }
+            }
+            if(chkKeyCrrsInc != null){
+                IDocument prntDocument = this.getEngDocumentByNumber(ses, prjCode, chkKeyCrrsInc);
+                this.log.info("Parent (CRRS) Doc ? " + prntDocument);
+                if (prntDocument != null && prntDocument.getDescriptorValue("ccmPrjDocCategory").trim().equalsIgnoreCase("TRANSMITTAL")) {
+                    if (!Objects.equals(prntDocument.getID(), engDocument.getID())) {
+                        ILink lnk2 = srv.createLink(ses, prntDocument.getID(), (INodeGeneric) null, engDocument.getID());
+                        lnk2.commit();
+                        this.log.info("Created Link...");
+                    }
+                }
+            }
+            if(chkKeyCrrsOut != null){
+                IDocument prntDocument = this.getEngDocumentByNumber(ses, prjCode, chkKeyCrrsOut);
+                this.log.info("Parent (CRRS) Doc ? " + prntDocument);
+                if (prntDocument != null && prntDocument.getDescriptorValue("ccmPrjDocCategory").trim().equalsIgnoreCase("TRANSMITTAL")) {
+                    if (!Objects.equals(prntDocument.getID(), engDocument.getID())) {
+                        ILink lnk2 = srv.createLink(ses, prntDocument.getID(), (INodeGeneric) null, engDocument.getID());
+                        lnk2.commit();
+                        this.log.info("Created Link...");
+                    }
+                }
+            }
+        }
+    }
+
+    public IQueryDlg findQueryDlgForQueryClass(IQueryClass queryClass) {
+        IQueryDlg dlg = null;
+        if (queryClass != null) {
+            dlg = queryClass.getQueryDlg("default");
+        }
+
+        return dlg;
+    }
+    public IQueryParameter query(ISession session, IQueryDlg queryDlg, Map<String, String> descriptorValues) {
+        IDocumentServer documentServer = session.getDocumentServer();
+        ISerClassFactory classFactory = documentServer.getClassFactory();
+        IQueryParameter queryParameter = null;
+        IQueryExpression expression = null;
+        IComponent[] components = queryDlg.getComponents();
+
+        for(int i = 0; i < components.length; ++i) {
+            if (components[i].getType() == IMaskedEdit.TYPE) {
+                IControl control = (IControl)components[i];
+                String descriptorId = control.getDescriptorID();
+                String value = (String)descriptorValues.get(descriptorId);
+                if (value != null && value.trim().length() > 0) {
+                    IDescriptor descriptor = documentServer.getDescriptor(descriptorId, session);
+                    IQueryValueDescriptor queryValueDescriptor = classFactory.getQueryValueDescriptorInstance(descriptor);
+                    queryValueDescriptor.addValue(value);
+                    IQueryExpression expr = queryValueDescriptor.getExpression();
+                    if (expression != null) {
+                        expression = classFactory.getExpressionInstance(expression, expr, 0);
+                    } else {
+                        expression = expr;
+                    }
+                }
+            }
+        }
+
+        if (expression != null) {
+            queryParameter = classFactory.getQueryParameterInstance(session, queryDlg, expression);
+        }
+
+        return queryParameter;
+    }
+    public IDocumentHitList executeQuery(ISession session, IQueryParameter queryParameter) {
+        IDocumentServer documentServer = session.getDocumentServer();
+        return documentServer.query(queryParameter, session);
+    }
+    public IDocument getEngDocument(ISession session, String prjCode, String docKey) throws IOException {
+        IDocument result = null;
+        IDocumentServer documentServer = session.getDocumentServer();
+        this.descriptor1 = documentServer.getDescriptorForName(session, "ccmPRJCard_code");
+        this.descriptor2 = documentServer.getDescriptorForName(session, this.nameDescriptor1);
+        IQueryClass queryClass = documentServer.getQueryClassByName(session, this.searchClassName);
+        IQueryDlg queryDlg = this.findQueryDlgForQueryClass(queryClass);
+        Map<String, String> searchDescriptors = new HashMap();
+        searchDescriptors.put(this.descriptor1.getId(), prjCode);
+        searchDescriptors.put(this.descriptor2.getId(), docKey);
+        IQueryParameter queryParameter = this.query(session, queryDlg, searchDescriptors);
+        if (queryParameter != null) {
+            IDocumentHitList hitresult = this.executeQuery(session, queryParameter);
+            IDocument[] hits = hitresult.getDocumentObjects();
+            queryParameter.close();
+            return hits != null && hits.length > 0 ? hits[0] : null;
+        } else {
+            return null;
+        }
+    }
+    public IDocument getEngDocumentByNumber(ISession session, String prjCode, String docKey) {
+        IDocument result = null;
+        log.info("Search Eng Document By PRJ Code:" + prjCode);
+        log.info("Search Eng Document By Number:" + docKey);
+        IDocumentServer documentServer = session.getDocumentServer();
+        this.descriptor1 = documentServer.getDescriptorForName(session, "ccmPRJCard_code");
+        this.descriptor2 = documentServer.getDescriptorForName(session, "ccmPrjDocNumber");
+        IQueryClass queryClass = documentServer.getQueryClassByName(session, this.searchClassName);
+        IQueryDlg queryDlg = this.findQueryDlgForQueryClass(queryClass);
+        Map<String, String> searchDescriptors = new HashMap();
+        searchDescriptors.put(this.descriptor1.getId(), prjCode);
+        searchDescriptors.put(this.descriptor2.getId(), docKey);
+        IQueryParameter queryParameter = this.query(session, queryDlg, searchDescriptors);
+        if (queryParameter != null) {
+            IDocumentHitList hitresult = this.executeQuery(session, queryParameter);
+            IDocument[] hits = hitresult.getDocumentObjects();
+            queryParameter.close();
+            return hits != null && hits.length > 0 ? hits[0] : null;
+        } else {
+            return null;
+        }
+    }
+    public IDocument checkDublicateEngDocByFileName(IDocument doc1){
+        IDocument result = null;
+        ISession session = this.getSes();
+        String searchClassName = "Search Engineering Documents";
+        IDocumentServer documentServer = session.getDocumentServer();
+        IDescriptor descriptor1 = documentServer.getDescriptorForName(session, "ccmPRJCard_code");
+        IDescriptor descriptor2 = documentServer.getDescriptorForName(session, "ccmPrjDocFileName");
+        IQueryClass queryClass = documentServer.getQueryClassByName(session, searchClassName);
+        IQueryDlg queryDlg = this.findQueryDlgForQueryClass(queryClass);
+        Map<String, String> searchDescriptors = new HashMap();
+        searchDescriptors.put(descriptor1.getId(), doc1.getDescriptorValue("ccmPRJCard_code"));
+        searchDescriptors.put(descriptor2.getId(), doc1.getDescriptorValue("ccmPrjDocFileName"));
+        IQueryParameter queryParameter = this.query(session, queryDlg, searchDescriptors);
+        if (queryParameter != null) {
+            IDocumentHitList hitresult = this.executeQuery(session, queryParameter);
+            IDocument[] hits = hitresult.getDocumentObjects();
+            queryParameter.close();
+            for(IDocument ldoc : hits){
+                String docID = doc1.getID();
+                String chkID = ldoc.getID();
+                if(!Objects.equals(docID, chkID)){
+                    result = ldoc;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    public static Object getValue(Cell cell, CellType type) {
+        switch (type) {
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    return getLocalDateTime(cell.getDateCellValue().toString());
+                } else {
+                    double value = cell.getNumericCellValue();
+                    if (value == Math.floor(value)) {
+                        return (long)value;
+                    }
+
+                    return value;
+                }
+            case STRING:
+                return cell.getStringCellValue();
+            case FORMULA:
+                return getValue(cell, cell.getCachedFormulaResultType());
+            case BOOLEAN:
+                return cell.getBooleanCellValue();
+            case _NONE:
+                return null;
+            case BLANK:
+                return null;
+            case ERROR:
+                return null;
+            default:
+                return null;
+        }
+    }
+    public static LocalDateTime getLocalDateTime(String strDate) {
+        strDate = strDate.replace("TRT", "Europe/Istanbul");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzzz yyyy");
+        ZonedDateTime zdt = ZonedDateTime.parse(strDate, formatter);
+        LocalDateTime ldt = zdt.toLocalDateTime();
+        return ldt;
+    }
+
+}
---
+++ src/test/groovy/junit/ExampleTests.groovy
@@ -0,0 +1,62 @@
+package junit
+
+import de.ser.doxis4.agentserver.AgentExecutionResult
+import eng.ser.com.UpdateLinks
+import org.junit.*
+
+class ExampleTests {
+
+    Binding binding
+
+    @BeforeClass
+    static void initSessionPool() {
+        AgentTester.initSessionPool()
+    }
+
+    @Before
+    void retrieveBinding() {
+        binding = AgentTester.retrieveBinding()
+    }
+
+    @Test
+    void testForAgentResult() {
+        def agent = new UpdateLinks();
+
+        //String ids ="SD06D_QCON24859b3414-ee73-4151-9b0d-0e5b93edf494182023-10-13T14:28:26.483Z011"
+        String ids ="SD06D_QCON2487db58c0-389c-468a-9de9-635cda71054c182023-12-21T10:07:33.909Z011"
+
+
+
+        binding["AGENT_EVENT_OBJECT_CLIENT_ID"] = ids
+
+        def result = (AgentExecutionResult)agent.execute(binding.variables)
+        assert result.resultCode == 0
+    }
+
+    /*
+        def agent = new GroovyAgent()
+        binding["AGENT_EVENT_OBJECT_CLIENT_ID"] = "SD0bGENERIC_DOC24d79641cb-c437-4e39-81af-3051824baa25182021-01-14T10:09:04.857Z011"
+        def result = (AgentExecutionResult)agent.execute(binding.variables)
+        assert result.resultCode == 0
+        assert result.executionMessage.contains("Linux")
+        assert agent.eventInfObj instanceof IDocument
+     */
+
+
+    @Test
+    void testForJavaAgentMethod() {
+        //def agent = new JavaAgent()
+        //agent.initializeGroovyBlueline(binding.variables)
+        //assert agent.getServerVersion().contains("Linux")
+    }
+
+    @After
+    void releaseBinding() {
+        AgentTester.releaseBinding(binding)
+    }
+
+    @AfterClass
+    static void closeSessionPool() {
+        AgentTester.closeSessionPool()
+    }
+}
